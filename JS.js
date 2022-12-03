7671c50c26757f8c09d25bd656ce8959bf32a65d
MubangaNgosa <Mubangacbngosa@gmail.com>
2022-12-02 16:28:56 -0800

    adding waffle chart

--- JS.js
+++ JS.js
@@ -1,9 +1,10 @@
-// set dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 90, left: 50},
-    width = 9936 - margin.left - margin.right,
+// set the dimensions and margins of the graph
+var margin = {top: 80, right: 25, bottom: 30, left: 40},
+    width = 450 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
-var svg = d3.select("#barGraphViz")
+// append the svg object to the body of the page
+var svg = d3.select("#myWaffleChart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -11,47 +12,106 @@ var svg = d3.select("#barGraphViz")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-// Read data
-d3.csv("data.csv", function(data) {
+//Read the data
+d3.csv("data1.csv", function(data) {
 
-    // Add X axis
+    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+    var myGroups = d3.map(data, function(d){return d.group;}).keys()
+    var myVars = d3.map(data, function(d){return d.variable;}).keys()
+
+    // Build X scales and axis:
     var x = d3.scaleBand()
-        .range([0, width])
-        .domain(data.map(function(d) { return d.Country; }))
-        .padding(0.2);
+        .range([ 0, width ])
+        .domain(myGroups)
+        .padding(0.05);
     svg.append("g")
+        .style("font-size", 15)
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+        .call(d3.axisBottom(x).tickSize(0))
+        .select(".domain").remove()
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, 500])
-        .range([ height, 0]);
+    // Build Y scales and axis:
+    var y = d3.scaleBand()
+        .range([ height, 0 ])
+        .domain(myVars)
+        .padding(0.05);
     svg.append("g")
-        .call(d3.axisLeft(y));
+        .style("font-size", 15)
+        .call(d3.axisLeft(y).tickSize(0))
+        .select(".domain").remove()
 
-    // Bars
-    svg.selectAll("myBar")
-        .data(data)
+    // Build color scale
+    var myColor = d3.scaleSequential()
+        .interpolator(d3.interpolateInferno)
+        .domain([1,100])
+
+    // create a tooltip
+    var tooltip = d3.select("#myWaffleChart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+        tooltip
+            .style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
+    var mousemove = function(d) {
+        tooltip
+            .html("The exact value of<br>this cell is: " + d.value)
+            .style("left", (d3.mouse(this)[0]+70) + "px")
+            .style("top", (d3.mouse(this)[1]) + "px")
+    }
+    var mouseleave = function(d) {
+        tooltip
+            .style("opacity", 0)
+        d3.select(this)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+    }
+
+    // add the squares
+    svg.selectAll()
+        .data(data, function(d) {return d.group+':'+d.variable;})
         .enter()
         .append("rect")
-        .attr("x", function(d) { return x(d.Country); })
-        .attr("width", x.bandwidth())
-        .attr("fill", "#69b3a2")
-        // no bar at the beginning thus:
-        .attr("height", function(d) { return height - y(0); }) // always equal to 0
-        .attr("y", function(d) { return y(0); })
-
-    // Animation
-    svg.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("y", function(d) { return y(d.T_2015); })
-        .attr("height", function(d) { return height - y(d.T_2015); })
-        .delay(function(d,i){console.log(i) ; return(i*100)})
-
+        .attr("x", function(d) { return x(d.group) })
+        .attr("y", function(d) { return y(d.variable) })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) { return myColor(d.value)} )
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
 })
 
+// Add title to graph
+svg.append("text")
+    .attr("x", 0)
+    .attr("y", -50)
+    .attr("text-anchor", "left")
+    .style("font-size", "22px")
+    .text("A d3.js heatmap");
+
+// Add subtitle to graph
+svg.append("text")
+    .attr("x", 0)
+    .attr("y", -20)
+    .attr("text-anchor", "left")
+    .style("font-size", "14px")
+    .style("fill", "grey")
+    .style("max-width", 400)
+    .text("A short description of the take-away message of this chart.");
